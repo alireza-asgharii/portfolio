11464facfb04a9cfd0aadaa436c67932e74bea27
Alireza Asghari <aa9967316@gmail.com>
2024-10-21 13:05:49 +0330

    ⚰️ Remove dead code

--- src/components/module/Grid.tsx
+++ src/components/module/Grid.tsx
@@ -1,42 +1,42 @@
 import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "@/components/ui/BentoGrid";
 
-type classNameType = Record<
-  number,
-  {
-    id: number;
-    className: string;
-  }
->;
+// type classNameType = Record<
+//   number,
+//   {
+//     id: number;
+//     className: string;
+//   }
+// >;
 
 const Grid = () => {
-  const classNames: classNameType = {
-    1: {
-      id: 1,
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    },
-    2: {
-      id: 2,
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    },
-    3: {
-      id: 3,
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    },
-    4: {
-      id: 4,
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    },
+  // const classNames: classNameType = {
+  //   1: {
+  //     id: 1,
+  //     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+  //   },
+  //   2: {
+  //     id: 2,
+  //     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+  //   },
+  //   3: {
+  //     id: 3,
+  //     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+  //   },
+  //   4: {
+  //     id: 4,
+  //     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+  //   },
 
-    5: {
-      id: 5,
-      className: "md:col-span-3 md:row-span-2",
-    },
-    6: {
-      id: 6,
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    },
-  };
+  //   5: {
+  //     id: 5,
+  //     className: "md:col-span-3 md:row-span-2",
+  //   },
+  //   6: {
+  //     id: 6,
+  //     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+  //   },
+  // };
   return (
     <section id="about" className="">
       <BentoGrid className="grid">
--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { useState } from "react";
-import { IoCopyOutline } from "react-icons/io5";
 
 // Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
